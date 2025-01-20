c3fd52b1c386f59ef176533be3ec45055c135f67
Addin Munawwar <moonawar@protonmail.com>
2025-01-20 21:41:51 +0700

    chore: removing unused tailwind extension + add some size extenstion

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,13 +7,13 @@ export default {
 				'main-red'	: '#BA3A2C',
 				'main-black': '#222222',
 				'main-light': '#F6F4F1',
-				'other-gray': '#D9D9D9',
+				'main-gray': '#D9D9D9',
 			},
 			rotate: {
 				'135': '135deg',
 			},
-			fontSize: {
-				'xxs': '0.5rem',
+			minHeight: {
+				'1.25s': '125vh',
 			}
 		},
 	},
