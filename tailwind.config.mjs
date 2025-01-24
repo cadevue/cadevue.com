80c19a72b2c80024db2000cedea77846f8027810
Addin Munawwar <moonawar@protonmail.com>
2025-01-24 15:34:51 +0700

    feat: adjust contrast for accessibility

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,7 @@ export default {
 			},
 			colors: {
 				'main-red'	: '#BA3A2C',
-				'light-red'	: '#CA3A2A',
+				'light-red'	: '#FF432F',
 				'main-black': '#202020',
 				'main-light': '#F6F4F1',
 				'main-gray': '#D9D9D9',
