fbc10f500d6be8f1b514968826bfaef6e8f6bc16
Addin Munawwar <moonawar@protonmail.com>
2025-01-24 15:46:57 +0700

    chore: adjust color scheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,7 @@ export default {
 			},
 			colors: {
 				'main-red'	: '#BA3A2C',
-				'light-red'	: '#FF432F',
+				'light-red'	: '#FA4430',
 				'main-black': '#202020',
 				'main-light': '#F6F4F1',
 				'main-gray': '#D9D9D9',
