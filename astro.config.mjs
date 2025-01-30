d4dc10f5c3f4ccd1c4c511bd2eb8b86b368ded29
Addin Munawwar <moonawar@protonmail.com>
2025-01-30 14:50:16 +0700

    feat: seo - add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), sitemap()],
   prefetch: {
     defaultStrategy: 'viewport',
     prefetchAll: true,
-  }
+  },
+  site: 'https://cadevue.com',
 });
